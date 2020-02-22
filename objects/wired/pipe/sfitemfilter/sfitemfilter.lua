48c6dde772232075711960d368be6327e5ec8290
Phantomas <phantomas@phantomas.xyz>
2020-02-22 14:50:35 +0100

    [FIX] Fix item filter

--- objects/wired/pipe/sfitemfilter/sfitemfilter.lua
+++ objects/wired/pipe/sfitemfilter/sfitemfilter.lua
@@ -25,9 +25,13 @@ function showFail()
 end
 
 function beforeItemPush(item, nodeId)
-    if self.filterCount > 0 then
-        if self.filter[item.name] then
-            return peekPushItem(self.connectionMap[nodeId], item)
+    if self.filterCount > 0 and self.filter[item.name] then
+        for _, filtItem in pairs(self.filter[item.name]) do
+            if sfutil.compare(filtItem, item, self.ignoreFields) then
+                local ret = peekPushItem(self.connectionMap[nodeId], item)
+
+                if ret then return ret[2] end
+            end
         end
     end
 
@@ -37,65 +41,76 @@ end
 function onItemPush(item, nodeId)
     local pushResult = nil
 
-    if self.filterCount > 0 then
-        if self.filter[item.name] then
-            local peek = peekPushItem(self.connectionMap[nodeId], item)
+    if self.filterCount > 0 and self.filter[item.name] then
+        for _, filtItem in pairs(self.filter[item.name]) do
+            if sfutil.compare(filtItem, item, self.ignoreFields) then
+                local peek = peekPushItem(self.connectionMap[nodeId], item)
 
-            if peek then
-                pushResult = pushItem(self.connectionMap[nodeId], peek[1])
+                if peek then
+                    pushResult = pushItem(self.connectionMap[nodeId], peek[1])
+
+                    if pushResult then
+                        showPass()
+
+                        return pushResult[2]
+                    end
+                end
             end
         end
     end
 
-    if pushResult then
-        showPass()
-
-        return pushResult[2]
-    else
-        showFail()
-    end
-
+    showFail()
     return pushResult
 end
 
-function beforeItemPull(filter, nodeId)
-    if self.filterCount > 0 then
-        local pullFilter = {}
-        local filterMatch = false
-        for filterString, amount in pairs(filter) do
-            if self.filter[filterString] then
-                pullFilter[filterString] = amount
-                filterMatch = true
-            end
-        end
+local function mergeFilters(filters)
+    local filterMatch = false
+    local pullFilter = {}
 
-        if filterMatch then
-            return peekPullItem(self.connectionMap[nodeId], pullFilter)
+    if self.filterCount > 0 and filters then
+        for _, filter in pairs(filters) do
+            local filtItem = filter.item
+            if self.filter[filtItem.name] then
+                for _, item in pairs(self.filter[filtItem.name]) do
+                    if sfutil.compare(filtItem, item, self.ignoreFields) then
+                        pullFilter[#pullFilter + 1] = filter
+                        filterMatch = true
+                        break
+                    end
+                end
+            end
         end
     end
 
-    return false
+    return filterMatch, pullFilter
 end
 
-function onItemPull(filter, nodeId)
+function beforeItemPull(filters, nodeId)
+    local match, pullFilters = mergeFilters(filters)
+
+    if match then
+        ret = peekPullItem(self.connectionMap[nodeId], pullFilters)
+
+        if ret then return ret[2] end
+    end
+
+    return nil
+end
+
+function onItemPull(item, nodeId)
     local pullResult = nil
+    local filters = {{
+        item = item,
+        amount = {item.count, item.count}
+    }}
 
-    if self.filterCount > 0 then
-        local pullFilter = {}
-        local filterMatch = false
-        for filterString, amount in pairs(filter) do
-            if self.filter[filterString] then
-                pullFilter[filterString] = amount
-                filterMatch = true
-            end
-        end
+    local match, pullFilters = mergeFilters(filters)
 
-        if filterMatch then
-            local peek = peekPushItem(self.connectionMap[nodeId], pullFilter)
+    if match then
+        local peek = peekPushItem(self.connectionMap[nodeId], pullFilters)
 
-            if peek then
-                pullResult = pullItem(self.connectionMap[nodeId], peek[1])
-            end
+        if peek then
+            pullResult = pullItem(self.connectionMap[nodeId], peek[1])
         end
     end
 
@@ -113,15 +128,17 @@ end
 function buildFilter()
     self.filter = {}
     self.filterCount = 0
+    self.ignoreFields = { count = true, sfdist = true }
     local contents = world.containerItems(entity.id())
+
     if contents then
         for key, item in pairs(contents) do
-            if self.filter[item.name] then
-                self.filter[item.name] = math.min(self.filter[item.name], item.count)
-            else
-                self.filter[item.name] = item.count
-                self.filterCount = self.filterCount + 1
-            end
+            self.filter[item.name] = self.filter[item.name] or {}
+
+            local size = #self.filter[item.name]
+            
+            self.filter[item.name][size + 1] = item
+            self.filterCount = self.filterCount + 1
         end
     end
 
