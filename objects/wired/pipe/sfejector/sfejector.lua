c29dda4d5c8c56c13c6036b92d9ece2ca466e263
Phantomas <phantomas@phantomas.xyz>
2020-02-22 14:46:58 +0100

    [FIX] Fix sfejector orientation

--- objects/wired/pipe/sfejector/sfejector.lua
+++ objects/wired/pipe/sfejector/sfejector.lua
@@ -1,41 +1,53 @@
+local function rotate()
+    local position = entity.position()
+    local checkDirs = {}
+    checkDirs[1] = {-1, 0}
+    checkDirs[2] = {0, -1}
+    checkDirs[3] = {1, 0}
+    checkDirs[4] = {0, 1}
+
+    local angles = {}
+    for i = 0, 3 do
+        angles[i + 1] = (math.pi / 2) * i
+    end
+
+    if next(pipes.nodeEntities["item"]) then
+        for i = 1, 4 do
+            if next(pipes.nodeEntities["item"][i]) then
+                animator.rotateGroup("ejector", angles[i])
+                self.usedNode = i
+                return
+            end
+        end
+    end
+
+    -- No entity were found, or pipes.nodeEntities was not properlu set up. Fallback on pipes
+    for i = 1, 4 do
+        local tilePos = {position[1] + checkDirs[i][1], position[2] + checkDirs[i][2]}
+        local pipeDirections = pipes.getPipeTileData("item", tilePos, "foreground")
+
+        if pipeDirections then
+            animator.rotateGroup("ejector", angles[i])
+            self.usedNode = i
+            return
+        end
+    end
+end
+
 function init(virtual)
     pipes.init({itemPipe})
 
     self.dropPoint = {entity.position()[1] + 0.5, entity.position()[2] + 0.5} --TODO: Temporarily spawn inside until someone bothers adding several drop points based on orientation
 
     self.usedNode = 0
+    rotate()
 end
 
 --------------------------------------------------------------------------------
 function update(dt)
     pipes.update(dt)
 
-    local position = entity.position()
-    local checkDirs = {}
-    checkDirs[0] = {-1, 0}
-    checkDirs[1] = {0, -1}
-    checkDirs[2] = {1, 0}
-    checkDirs[3] = {0, 1}
-
-    if #pipes.nodeEntities["item"] > 0 then
-        for i=0,3 do 
-            local angle = (math.pi / 2) * i
-            if #pipes.nodeEntities["item"][i+1] > 0 then
-                animator.rotateGroup("ejector", angle)
-                self.usedNode = i + 1
-            end
-        end
-    elseif i == 3 then --Not connected to an object, look for pipes instead
-        for i=0,3 do 
-            local angle = (math.pi / 2) * i
-            local tilePos = {position[1] + checkDirs[i][1], position[2] + checkDirs[i][2]}
-            local pipeDirections = pipes.getPipeTileData("item", tilePos, "foreground")
-            if pipeDirections then
-                animator.rotateGroup("ejector", angle)
-                self.usedNode = i + 1
-            end
-        end
-    end
+    rotate()
 end
 
 function beforeItemPush(item, nodeId)
